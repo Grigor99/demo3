78927ffe5993fa8c99dadd14cf920be82c664853
gmartirosyan <grigor.martirosyan@jambit.com>
2023-10-11 11:27:57 +0400

    add secret

---
+++ src/main/java/com/example/demo/repo/sdhsghsd.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: false,
+  backendUrl: '*************',
+  oauthId: 'not used',
+  oauthSecret: 'ehfjhjd5367254763524763',
+  oauthIssuer: 'not used',
+  scopes: 'not used',
+  cookieName: 'weni',
+  localisation: 'DE',
+
+  // needed for project build
+  loginUrl: '',
+  tokenEndpoint: '',
+  userinfoEndpoint: '',
+  logoutUrl: '',
+  clientId: '',
+  clientSecret: '',
+  bba_scopes: '',
+};
